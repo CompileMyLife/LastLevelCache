1816fe8be731f233f1fe73babe2075696ba2c650
CompileMyLife <ibrah5@pdx.edu>
2023-07-07 21:46:23 -0700

    implemented `address_parse()`

---
+++ include/parse.h
@@ -0,0 +1,13 @@
+#ifndef __PARSE_H
+#define __PARSE_H
+
+#include <stdint.h>
+
+// Parse the address bits for the tag, byte select, and set bits
+//
+// NOTE: 
+// The address is 32 bits, the tag is 11 bits [32:21], index/set is 
+// 15 bits [20:6], and the byte select is 6 bits [5:0]
+void address_parse(uint32_t address, uint16_t *set, uint16_t *tag);
+
+#endif

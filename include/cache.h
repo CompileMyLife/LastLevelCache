bdd909e3690a86485fb16206ac9aac4013aa0e12
CompileMyLife <ibrah5@pdx.edu>
2023-07-11 01:33:42 -0700

    added `Set` structure and functions to create and initialize cache

--- include/cache.h
+++ include/cache.h
@@ -1,14 +1,43 @@
 #ifndef __CACHE_H
 #define __CACHE_H
 
+#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
 
-typedef struct cache_stats {
+typedef struct __cache_stats {
     uint64_t reads;
     uint64_t writes;
     uint64_t hits;
     uint64_t misses;
     float hit_ratio;
-} llc_stats;
+} Cache_Stats;
+
+typedef struct __set {
+    uint8_t line0[64];
+    uint8_t line1[64];
+    uint8_t line2[64];
+    uint8_t line3[64];
+    uint8_t line4[64];
+    uint8_t line5[64];
+    uint8_t line6[64];
+    uint8_t line7[64];
+
+    uint16_t tag[8];
+    uint8_t  valid_b;
+    uint8_t  dirty_b; 
+    uint8_t  LRU_b; 
+} Set;
+
+Set* create_cache(void);
+
+void delete_cache(Set* _cache);
+
+void init_cache(Set* _cache, Cache_Stats* _cache_stats);
+
+bool cache_read(uint32_t address);
+
+bool cache_write(uint32_t address);
 
 #endif
--- src/cache.c
+++ src/cache.c
@@ -1,1 +1,25 @@
 #include "cache.h"
+
+Set* create_cache(void) {
+    Set* cache;
+
+    // Allocate an array of 32768 sets and initalize each member in set to 0s
+    cache = (Set *)calloc(32768, sizeof(Set));
+    
+    return cache; 
+}
+
+void delete_cache(Set* cache) {
+    free(cache);
+}
+
+void init_cache(Set* _cache, Cache_Stats* _cache_stats) {
+    // Initialize all stats variables
+    _cache_stats->reads     = 0;
+    _cache_stats->writes    = 0;
+    _cache_stats->hits      = 0;
+    _cache_stats->misses    = 0;
+    _cache_stats->hit_ratio = 0;
+
+    // TODO: Initialize correct MESI and LRU bits for each sit in cache
+}

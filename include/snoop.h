a8aef02c6e9f4c375d430923ea4e8adb0cd319c3
CompileMyLife <ibrah5@pdx.edu>
2023-07-20 23:46:16 -0700

    renamed `NOHIT` to `MISS`

--- include/snoop.h
+++ include/snoop.h
@@ -7,7 +7,7 @@
 typedef enum _bus_ops{READ=1, WRITE, INVALIDATE, RWIM} bus_ops;
 
 // Snoop Result types
-typedef enum _snoop_results{NOHIT, HIT, HITM} snoop_results;
+typedef enum _snoop_results{MISS, HIT, HITM} snoop_results;
 
 // L2 to L1 message types
 typedef enum _L2_to_L1_msg{GETLINE=1, SENDLINE, INVALIDATELINE, EVICTLINE} L2_to_L1_msg;

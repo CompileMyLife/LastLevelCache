64a13522a59ebbd00a218dc88b542c34cad175cc
CompileMyLife <ibrah5@pdx.edu>
2023-07-20 23:44:01 -0700

    added `byte_sel` in case to be used later in system

--- include/parse.h
+++ include/parse.h
@@ -3,11 +3,17 @@
 
 #include <stdint.h>
 
+uint32_t addr;
+
+uint16_t tag;
+uint16_t set;
+uint8_t byte_sel;
+
 // Parse the address bits for the tag, byte select, and set bits
 //
 // NOTE: 
 // The address is 32 bits, the tag is 11 bits [32:21], index/set is 
 // 15 bits [20:6], and the byte select is 6 bits [5:0]
-void address_parse(uint32_t* address, uint16_t* set, uint16_t* tag);
+void address_parse(uint32_t* address, uint16_t* _set, uint16_t* _tag, uint8_t* _byte_sel);
 
 #endif
--- src/parse.c
+++ src/parse.c
@@ -1,13 +1,17 @@
 #include "parse.h"
 
-void address_parse(uint32_t* address, uint16_t* set, uint16_t* tag) {
+void address_parse(uint32_t* address, uint16_t* _set, uint16_t* _tag, uint8_t* _byte_sel) {
     // set to 0 initially
-    *tag = 0;
-    *set = 0;
+    *_tag      = 0;
+    *_set      = 0;
+    *_byte_sel = 0;
 
     // Tag bits are [32:21]
-    *tag = (*address & 0xFFE00000) >> 21;
+    *_tag = (*address & 0xFFE00000) >> 21;
 
     // Set bits are [20:6]
-    *set = (*address & 0x1FFFC0) >> 6;
+    *_set = (*address & 0x1FFFC0) >> 6;
+
+    // Byte Select bits are [5:0]
+    *_byte_sel = (*address & 0x0000001F);
 }

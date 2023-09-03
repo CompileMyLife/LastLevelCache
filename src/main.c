e0b4b56e09b36f504a32b8d6e70ba3994dea9446
CompileMyLife <ibrah5@pdx.edu>
2023-09-03 00:44:11 -0700

    added rest of `n` mode functions and end summary of cache statistics

--- src/main.c
+++ src/main.c
@@ -7,16 +7,15 @@
 #include "cache.h"
 #include "ops.h"
 
-#define DEBUG 1
+#define DEBUG 0
 
-// n-mode
-uint8_t mode = 0;
+// Globally declared for other includes to use
+bool normal = 1, silent = 0;
+
 
 int main(int argc, char *argv[]) { // Store option from CLI
     int opt;
 
-    // Flags; by default running in Normal Mode
-    bool normal = 1, silent = 0;
     bool file_flg = 0;
 
     char* file_name = NULL;
@@ -60,6 +59,10 @@ int main(int argc, char *argv[]) { // Store option from CLI
 
         // Initialize the cache to specified state and statistics to 0s
         init_cache(Cache, &cache_stats);
+        
+        // n-mode and address
+        uint8_t mode = 0;
+        uint32_t addr = 0;
 
         FILE *fp = fopen(file_name, "r");
         if (fp == NULL) {
@@ -75,7 +78,31 @@ int main(int argc, char *argv[]) { // Store option from CLI
             
             switch(mode) {
                 case 0:
-                    mode_0(Cache, &cache_stats, &tag, &set, &byte_sel);
+                    mode_0(Cache, &cache_stats, &addr, &tag, &set);
+                    break;
+                case 1:
+                    mode_1(Cache, &cache_stats, &addr, &tag, &set);
+                    break;
+                case 2:
+                    mode_2(Cache, &cache_stats, &addr, &tag, &set);
+                    break;
+                case 3:
+                    mode_3(Cache, &cache_stats, &addr, &tag, &set);
+                    break;
+                case 4:
+                    mode_4(Cache, &cache_stats, &addr, &tag, &set);
+                    break;
+                case 5:
+                    mode_5(Cache, &cache_stats, &addr, &tag, &set);
+                    break;
+                case 6:
+                    mode_6(Cache, &cache_stats, &addr, &tag, &set);
+                    break;
+                case 8:
+                    mode_8(Cache);
+                    break;
+                case 9:
+                    mode_9(Cache);
                     break;
                 default:
                     fprintf(stderr, "ERROR: Invalid parsed command\n");
@@ -86,16 +113,11 @@ int main(int argc, char *argv[]) { // Store option from CLI
         }
         fclose(fp);
 
-#if (DEBUG)
-        for (int i = 0; i < 32768; i++){
-            printf("Cache[%d].LRU_b = %X\n", i, Cache[i].LRU_b);
-            for(int j = 0; j < 8; j++){
-                printf("Cache[%d].tag[%d]    = %X\n", i, j, Cache[i].tag[j]);
-                printf("Cache[%d].valid_b[%d] = %X\n", i, j, Cache[i].valid_b[j]);
-                printf("Cache[%d].MESI_b[%d] = %X\n", i, j, Cache[i].MESI_b[j]);
-            }
-        }
-#endif
+        cache_stats.hit_ratio = (double) cache_stats.hits / (double) (cache_stats.reads + cache_stats.writes);
+        cache_stats.miss_ratio = (double) cache_stats.misses / (double) (cache_stats.reads + cache_stats.writes);
+        
+        fprintf(stdout, "Total Memory Accesses = %ld, Reads = %ld, Writes = %ld, HITs = %ld, MISSes = %ld, Hit_ratio = %f, Miss_ratio = %f\n", cache_stats.reads + cache_stats.writes, cache_stats.reads, cache_stats.writes, cache_stats.hits, cache_stats.misses, cache_stats.hit_ratio, cache_stats.miss_ratio);
+
         delete_cache(Cache);
 
         exit(EXIT_SUCCESS);

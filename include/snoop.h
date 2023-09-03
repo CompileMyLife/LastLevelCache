561f2bc8777b083a1c0fea441376d7674514d371
CompileMyLife <ibrah5@pdx.edu>
2023-09-03 00:38:16 -0700

    add `normal and silent` variables referenced from `src/main.c` then changed parameters for `operate_bus(), msg_to_cache()` to use `char*` type for ease of use

--- include/snoop.h
+++ include/snoop.h
@@ -1,28 +1,33 @@
 #ifndef __SNOOP_H
 #define __SNOOP_H
 
+#include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+
+extern bool normal;
+extern bool silent;
 
 // Bus Operation types
-typedef enum _bus_ops{READ=1, WRITE, INVALIDATE, RWIM} bus_ops;
+//typedef enum _bus_ops{READ=1, WRITE, INVALIDATE, RWIM} bus_ops;
 
 // Snoop Result types
-typedef enum _snoop_results{MISS, HIT, HITM} snoop_results;
+typedef enum _snoop_results{NOHIT, HIT, HITM} snoop_results;
 
 // L2 to L1 message types
-typedef enum _L2_to_L1_msg{GETLINE=1, SENDLINE, INVALIDATELINE, EVICTLINE} L2_to_L1_msg;
+//typedef enum _L2_to_L1_msg{GETLINE=1, SENDLINE, INVALIDATELINE, EVICTLINE} L2_to_L1_msg;
 
 // Simulate a bus operation and to capture the snoop results of last level
 // caches of other processors
-void operate_bus(int bus_op, uint32_t address, int snoop_result);
+void operate_bus(char* op, uint32_t* address);
 
 // Simulate the report of snoop results by other caches
-void get_snoop_result(uint32_t address);
+snoop_results get_snoop_result(uint32_t* address);
 
 // Report the resutls of the snooping bus operations produced by other caches
-void put_snoop_result(uint32_t address, int snoop_result);
+void put_snoop_result(uint32_t* address, snoop_results result);
 
 // Simulate communication to higher level cache
-void msg_to_cache(int msg, uint32_t address);
+void msg_to_cache(char* msg, uint32_t* address);
 
 #endif
--- src/snoop.c
+++ src/snoop.c
@@ -1,1 +1,60 @@
 #include "snoop.h"
+
+void operate_bus(char* op, uint32_t* address) {
+    snoop_results result = get_snoop_result(address);
+
+    if (normal) {
+        switch (result) {
+            case NOHIT:
+                fprintf(stdout, "Bus Operation: %s, Address: %X, Snoop Result: NOHIT\n", 
+                    op, *address);
+                break;
+            case HITM:
+                fprintf(stdout, "Bus Operation: %s, Address: %X, Snoop Result: HITM\n", 
+                    op, *address);
+                break;
+            case HIT:
+            default:
+                fprintf(stdout, "Bus Operation: %s, Address: %X, Snoop Result: HIT\n", 
+                    op, *address);
+                break;
+        }
+    }
+}
+
+snoop_results get_snoop_result(uint32_t* address) {
+    *address = (*address  & 0x00000003); // mask the 1th and 0th bit 
+
+    switch(*address) {
+        case 1: 
+            return NOHIT;
+        case 2:
+            return HITM;
+        case 0:
+        default:
+            return HIT;
+    }
+}
+
+void put_snoop_result(uint32_t* address, snoop_results result) {
+    if (normal) {
+        switch (result) {
+            case NOHIT:
+                fprintf(stdout, "Snoop Result Address: %X, Snoop Result: NOHIT\n", *address);
+                break;
+            case HITM:
+                fprintf(stdout, "Snoop Result Address: %X, Snoop Result: HITM\n", *address);
+                break;
+            case HIT:
+            default:
+                fprintf(stdout, "Snoop Result Address: %X, Snoop Result: HIT\n", *address);
+                break;
+        }
+    }
+}
+
+void msg_to_cache(char* msg, uint32_t* address) {
+    if (normal) {
+        fprintf(stdout, "L2 Cache: %s %X\n", msg, *address);
+    }
+}

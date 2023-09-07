59609a631098079b90f7a381141779fa2578ac0e
CompileMyLife <ibrah5@pdx.edu>
2023-09-06 22:11:28 -0700

    made the `fprintf()` statements show the address as 32 bit number in hex

--- src/snoop.c
+++ src/snoop.c
@@ -6,16 +6,16 @@ void operate_bus(char* op, uint32_t* address) {
     if (normal) {
         switch (result) {
             case NOHIT:
-                fprintf(stdout, "Bus Operation: %s, Address: %X, Snoop Result: NOHIT\n", 
+                fprintf(stdout, "Bus Operation: %s, Address: %08X, Snoop Result: NOHIT\n", 
                     op, *address);
                 break;
             case HITM:
-                fprintf(stdout, "Bus Operation: %s, Address: %X, Snoop Result: HITM\n", 
+                fprintf(stdout, "Bus Operation: %s, Address: %08X, Snoop Result: HITM\n", 
                     op, *address);
                 break;
             case HIT:
             default:
-                fprintf(stdout, "Bus Operation: %s, Address: %X, Snoop Result: HIT\n", 
+                fprintf(stdout, "Bus Operation: %s, Address: %08X, Snoop Result: HIT\n", 
                     op, *address);
                 break;
         }
@@ -40,14 +40,14 @@ void put_snoop_result(uint32_t* address, snoop_results result) {
     if (normal) {
         switch (result) {
             case NOHIT:
-                fprintf(stdout, "Snoop Result Address: %X, Snoop Result: NOHIT\n", *address);
+                fprintf(stdout, "Snoop Result Address: %08X, Snoop Result: NOHIT\n", *address);
                 break;
             case HITM:
-                fprintf(stdout, "Snoop Result Address: %X, Snoop Result: HITM\n", *address);
+                fprintf(stdout, "Snoop Result Address: %08X, Snoop Result: HITM\n", *address);
                 break;
             case HIT:
             default:
-                fprintf(stdout, "Snoop Result Address: %X, Snoop Result: HIT\n", *address);
+                fprintf(stdout, "Snoop Result Address: %08X, Snoop Result: HIT\n", *address);
                 break;
         }
     }
@@ -55,6 +55,6 @@ void put_snoop_result(uint32_t* address, snoop_results result) {
 
 void msg_to_cache(char* msg, uint32_t* address) {
     if (normal) {
-        fprintf(stdout, "L2 Cache: %s %X\n", msg, *address);
+        fprintf(stdout, "L2 Cache: %s %08X\n", msg, *address);
     }
 }
